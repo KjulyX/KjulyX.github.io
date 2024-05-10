ebfe3616233c51b68b13c9b686389e3a1f714b61
Kjuly <dev@kjuly.com>
2024-05-10 22:37:46 +0800

    Rename build-site.sh to start.sh to handle build and serve

--- build-site.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-COMPOSE_FILE="compose-build-site.yaml"
-
-docker compose -f "$COMPOSE_FILE" up
-docker compose -f "$COMPOSE_FILE" down
---
+++ start.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -e
+
+nc='\033[0m'
+r='\033[0;31m'
+g='\033[0;32m'
+
+LANGS=(
+  "en"
+  "cn"
+)
+
+while [[ "$1" =~ ^"-" ]]; do
+  if [ "$1" = "-l" ]; then
+    shift 1; lang=$1
+  fi
+  shift 1
+done
+
+[ -z "$1" ] && action="serve" || action=$1
+
+if [ -z "$lang" ]; then
+  lang="en"
+elif [[ ! ${LANGS[*]} =~ $lang ]]; then
+  echo -e "\n  ERROR: Undefined lang: ${r}$lang${nc}\n" && exit 1
+fi
+echo -e "\n  ACTION: ${g}$action${nc}, LANG: ${g}$lang${nc}\n"
+
+# Provide extra options for jekyll when needed.
+[ "$lang" != "en" ] && jekyll_cmd_extra_options="--config _config.yml,$lang/_config.yml"
+
+# Docker compose
+if [ "$action" == "build" ]; then
+  COMPOSE_FILE="compose-build-site.yaml"
+
+  docker compose -f "$COMPOSE_FILE" up
+  docker compose -f "$COMPOSE_FILE" down
+else
+  JEKYLL_CMD_EXTRA_OPTIONS="$jekyll_cmd_extra_options" docker compose up
+fi
